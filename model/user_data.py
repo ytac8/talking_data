0927180dfa62342158e89de1594b7070a8bc6394
ytac8 <y.tac0223@gmail.com>
2018-04-20 15:46:06 +0900

    labelの追加

--- model/user_data.py
+++ model/user_data.py
@@ -15,22 +15,22 @@ class UserData(Dataset):
 
     def __getitem__(self, idx):
         user_id = self.user_lis
-        user_data = self._preprocess(user_id)
+        user_data, label = self._preprocess(user_id)
         padded = torch.from_numpy(self._padding(user_data))
-        label = "l"
         return {"feature": padded, "label": label}
 
     def _preprocess(self, user_id):
         df = pd.read_csv(
             '../data/preprocessed/user_data/data_' + str(self.user) + '.csv')
         df = df.sort_values(by='click_time')
+        label = df.is_attribute
         dummied_app = self.app.transform(df.app.tolist())
         dummied_os = self.os.transform(df.os.tolist())
         dummied_channel = self.channel.transform(df.channel.tolist())
         dummied_device = self.device.transform(df.device.tolist())
         mat = np.concatenate((dummied_app, dummied_channel,
                               dummied_os, dummied_device), axis=1)
-        return mat
+        return mat, label
 
     def _padding(self, feature, pad_value=-1):
         pad_len = self.max_len - len(feature)
